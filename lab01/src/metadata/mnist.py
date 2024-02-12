833349cb390ff4982b11a4a6e6928365cab5b4dd
Yann Billeter <bily@zhaw.ch>
2024-02-12 09:48:16 +0100

    Add missing dependencies to lab01

---
+++ lab01/src/metadata/mnist.py
@@ -0,0 +1,12 @@
+"""Metadata for the MNIST dataset."""
+
+import lib.metadata.shared as shared
+
+DOWNLOADED_DATA_DIRNAME = shared.DOWNLOADED_DATA_DIRNAME
+
+DIMS = (1, 28, 28)
+OUTPUT_DIMS = (1,)
+MAPPING = list(range(10))
+
+TRAIN_SIZE = 55000
+VAL_SIZE = 5000
